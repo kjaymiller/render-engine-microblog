3a5f0d0a94426490411e1cdeef4db17c34c30c7e
Jay Miller <kjaymiller@gmail.com>
2023-01-12 22:02:39 -0800

    first commit

---
+++ src/collection.py
@@ -0,0 +1,18 @@
+from dateutil import parser
+from render_engine.blog import Blog, BlogPost
+from render_engine.parsers import MarkdownPageParser
+
+class MicroBlogPost(BlogPost):
+    @property
+    def _slug(self):
+        base_date = parser.parse(self.date)
+        return base_date.strftime("%Y%m%d%H%M")
+
+    @property    
+    def _title(self):
+        return ""
+
+class MicroBlog(Blog):
+    template = "blog.html"
+    PageParser = MarkdownPageParser
+    content_type = MicroBlogPost
